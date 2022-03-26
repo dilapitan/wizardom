75b3a887195f0a45df7c058b178c81978b0105d5
dilapitan <domlapitan@gmail.com>
2022-03-26 17:37:15 +0800

    Add custom theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,25 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-});
+  theme: {
+    themes: {
+      light: {
+        primary: '#333F71',
+        secondary: '#46579B',
+        accent: '#D44C0C',
+        error: '#C1301D',
+        background: '#FFFFFF',
+      },
+      dark: {
+        primary: '#333F71',
+        secondary: '#46579B',
+        accent: '#D44C0C',
+        error: '#C1301D',
+        background: '#12181B',
+      },
+    },
+  },
+})
