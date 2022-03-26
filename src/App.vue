3d63dd7818547aee5658292ee3aa411a9f6f1311
dilapitan <domlapitan@gmail.com>
2022-03-26 17:49:55 +0800

    Add search icon

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <v-app>
     <v-app-bar app>
       <h2>Wizardom</h2>
-
       <v-spacer></v-spacer>
+      <v-btn icon color="primary" x-large>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
