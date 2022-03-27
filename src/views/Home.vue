ed66b28c33fd5eab8e2cadcb79c9ee01de06ac01
dilapitan <domlapitan@gmail.com>
2022-03-27 11:35:45 +0800

    Add Sort By in Projects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,34 @@
 <template>
   <v-container>
-    <p>List of Projects</p>
+    <br />
+
+    <h1 id="special-font" class="specialFont--text">LIST OF PROJECTS</h1>
+
+    <v-divider></v-divider>
+
+    <br />
+
+    <v-row>
+      <v-col sm="7" md="9"></v-col>
+      <v-col cols="9" sm="5" md="3">
+        <v-select
+          :items="items"
+          label="Sort By"
+          filled
+          rounded
+          dense
+        ></v-select>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'Home',
+
+  data: () => ({
+    items: ['Date Added (oldest)', 'Date Added (newest)'],
+  }),
 }
 </script>
