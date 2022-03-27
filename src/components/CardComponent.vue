cbec3bcd9016646a5e56938b3afe1d92b06e4af6
dilapitan <domlapitan@gmail.com>
2022-03-27 14:06:21 +0800

    Add initial cards

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card hover>
+    <v-img
+      :src="data.src"
+      class="align-end"
+      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+      height="250px"
+    >
+    </v-img>
+
+    <v-container>
+      <v-card-title>{{ data.title }}</v-card-title>
+      <v-card-text>
+        <div>
+          {{ data.description }}
+        </div>
+
+        <v-chip-group class="mt-2">
+          <v-chip v-for="tag in data.tags" :key="tag">
+            {{ tag }}
+          </v-chip>
+        </v-chip-group>
+      </v-card-text>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'CardComponent',
+  props: ['data'],
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,15 +20,73 @@
         ></v-select>
       </v-col>
     </v-row>
+
+    <v-row>
+      <v-col cols="12" sm="6" v-for="project in projects" :key="project.id">
+        <v-container>
+          <CardComponent :data="project" />
+        </v-container>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import CardComponent from '@/components/CardComponent'
+
 export default {
   name: 'Home',
 
+  components: {
+    CardComponent,
+  },
+
   data: () => ({
     items: ['Date Added (oldest)', 'Date Added (newest)'],
+    projects: [
+      {
+        id: 1,
+        src: 'https://picsum.photos/id/1/500/300',
+        title: 'Project 1',
+        description: 'This is Project 1',
+        tags: ['#vue', '#firebase'],
+      },
+      {
+        id: 2,
+        src: 'https://picsum.photos/id/2/500/300',
+        title: 'Project 2',
+        description: 'This is Project 2',
+        tags: ['#vue', '#firebase'],
+      },
+      {
+        id: 3,
+        src: 'https://picsum.photos/id/3/500/300',
+        title: 'Project 3',
+        description: 'This is Project 3',
+        tags: ['#css', '#flexbox'],
+      },
+      {
+        id: 4,
+        src: 'https://picsum.photos/id/4/500/300',
+        title: 'Project 4',
+        description: 'This is Project 4',
+        tags: ['#react', '#firebase'],
+      },
+      {
+        id: 5,
+        src: 'https://picsum.photos/id/5/500/300',
+        title: 'Project 5',
+        description: 'This is Project 5',
+        tags: ['#vue', '#node'],
+      },
+      {
+        id: 6,
+        src: 'https://picsum.photos/id/6/500/300',
+        title: 'Project 6',
+        description: 'This is Project 6',
+        tags: ['#react', '#node'],
+      },
+    ],
   }),
 }
 </script>
