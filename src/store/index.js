c3a6e67c06c8f0641ae6107cbe9d2d4242a35509
dilapitan <domlapitan@gmail.com>
2022-03-28 20:37:43 +0800

    Update Store by adding tags

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,51 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tags: [
+      {
+        id: 1,
+        label: '#vue',
+        backgroundColor: '#42b883',
+        textColor: '#12181B',
+      },
+      {
+        id: 2,
+        label: '#node',
+        backgroundColor: '#66BB6A',
+        textColor: '#12181B',
+      },
+      {
+        id: 3,
+        label: '#firebase',
+        backgroundColor: '#FFCB2A',
+        textColor: '#12181B',
+      },
+      {
+        id: 4,
+        label: '#react',
+        backgroundColor: '#00b3e6',
+        textColor: '#12181B',
+      },
+      {
+        id: 5,
+        label: '#css',
+        backgroundColor: '#F854BE',
+        textColor: '#12181B',
+      },
+      {
+        id: 6,
+        label: '#tailwind',
+        backgroundColor: '#39BDF8',
+        textColor: '#12181B',
+      },
+      {
+        id: 7,
+        label: '#flexbox',
+        backgroundColor: '#454E56',
+        textColor: '#FFFFFF',
+      },
+    ],
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  mutations: {},
+  actions: {},
 })
