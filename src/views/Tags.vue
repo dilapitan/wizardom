348dc226ea96e85cc50477f331a4f7c525608fa6
dilapitan <domlapitan@gmail.com>
2022-03-28 20:52:06 +0800

    Update Tags view content

--- src/views/Tags.vue
+++ src/views/Tags.vue
@@ -10,11 +10,42 @@
     <v-divider></v-divider>
 
     <br />
+
+    <div>
+      <TagComponent
+        class="ma-1"
+        v-for="(tag, i) in tags"
+        :key="i"
+        :value="tag.label"
+      />
+    </div>
+
+    <br />
   </v-container>
 </template>
 
 <script>
+import TagComponent from '@/components/TagComponent'
+
 export default {
   name: 'Tags',
+
+  components: {
+    TagComponent,
+  },
+
+  data: () => ({
+    tags: null,
+  }),
+
+  created() {
+    this.tags = this.getTags()
+  },
+
+  methods: {
+    getTags() {
+      return this.$store.state.tags
+    },
+  },
 }
 </script>
